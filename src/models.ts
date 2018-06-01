43451ae002921c24b67646e6d6a4772f8683fa6e
Yuyang Zhang <helloqiu95@gmail.com>
2018-06-01 17:50:42 +0800

    feat(model): add models

---
+++ src/models.ts
@@ -0,0 +1,82 @@
+import { isArray, isString } from 'util'
+
+/** 同一个 label 下包含的内容的配置 */
+export interface MatchConfig {
+  /** 对配置的描述 */
+  description: string,
+  /** 内容 */
+  content: Array<string>
+}
+
+/** 用 label 区分的不同类型的 issue 的配置 */
+export interface IssueConfig {
+  /** 对应的 label */
+  label: string
+  /** 所包含的检查内容和描述 */
+  items: Array<MatchConfig>
+}
+
+/** 最外层的 Config interface */
+export interface Config {
+  /** 用来记录所有的 issue 相关设置 */
+  issues: Array<IssueConfig>
+  /** 要关闭 github issue 时发的 comment */
+  comment: string
+}
+
+/**
+ * 判断一个 object 是不是 MatchConfig
+ */
+export function isMatchConfig (item: object | MatchConfig): item is MatchConfig {
+  if (!('description' in item
+  && 'content' in item
+  && isString(item.description)
+  && isArray(item.content)
+  && item.content.length > 0)) {
+    return false
+  }
+  for (const i of item.content) {
+    if (!isString(i)) {
+      return false
+    }
+  }
+  return true
+}
+
+/**
+ * 判断一个 object 是不是 IssueConfig
+ */
+export function isIssueConfig (item: object | IssueConfig): item is IssueConfig {
+  if (!('label' in item
+  && 'items' in item
+  && isString(item.label)
+  && isArray(item.items)
+  && item.items.length > 0)) {
+    return false
+  }
+  for (const i of item.items) {
+    if (!isMatchConfig(i)) {
+      return false
+    }
+  }
+  return true
+}
+/**
+ * 判断一个对象是不是 Config
+ */
+export function isConfig (item: object | Config): item is Config {
+  if (!('comment' in item
+  && 'issues' in item
+  && isString(item.comment)
+  && isArray(item.issues)
+  && item.issues.length > 0)) {
+    return false
+  }
+  for (const i of item.issues) {
+    if (!isIssueConfig(i)) {
+      return false
+    }
+  }
+  return true
+
+}
---
+++ test/models.test.ts
@@ -0,0 +1,75 @@
+import { isConfig, isIssueConfig, isMatchConfig } from '../src/models'
+import * as path from 'path'
+import exampleConfig from './example.config.json'
+
+describe('models', () => {
+
+  it('isConfig', () => {
+    expect(isConfig(exampleConfig)).toBeTruthy()
+
+    const emptyExample = {}
+    expect(isConfig(emptyExample)).toBeFalsy()
+
+    const wrongExample1 = { issues: [], comment: 'test' }
+    expect(isConfig(wrongExample1)).toBeFalsy()
+
+    const wrongExample2 = { comment: 'test' }
+    expect(isConfig(wrongExample2)).toBeFalsy()
+
+    const rightExample = {
+      issues: [{ label: 'test', items: [{ description: '233', content: ['123'] }] }],
+      comment: 'test'
+    }
+    expect(isConfig(rightExample)).toBeTruthy()
+  })
+
+  it('isIssueConfig', () => {
+    expect(isIssueConfig(exampleConfig.issues[0])).toBeTruthy()
+
+    const emptyExample = {}
+    expect(isIssueConfig(exampleConfig)).toBeFalsy()
+
+    const noItemsExample = { label: 'test' }
+    expect(isIssueConfig(noItemsExample)).toBeFalsy()
+
+    const noLabelExample = { items: [{ description: 'test', content: ['233'] }] }
+    expect(isIssueConfig(noLabelExample)).toBeFalsy()
+
+    const emptyItemsExample = { ...noItemsExample, items: [] }
+    expect(isIssueConfig(emptyItemsExample)).toBeFalsy()
+
+    const wrongItemsExample = { ...noItemsExample, items: [{ description: 'test' }] }
+    expect(isIssueConfig(wrongItemsExample)).toBeFalsy()
+
+    const wrongLabelExample = { ...noLabelExample, label: 233 }
+    expect(isIssueConfig(wrongLabelExample)).toBeFalsy()
+
+    const rightExample = { ...noItemsExample, ...noLabelExample }
+    expect(isIssueConfig(rightExample)).toBeTruthy()
+  })
+
+  it('isMatchConfig', () => {
+    expect(isMatchConfig(exampleConfig.issues[0].items[0])).toBeTruthy()
+
+    const emptyExample = {}
+    expect(isMatchConfig(emptyExample)).toBeFalsy()
+
+    const noContentExample = { description: 'test' }
+    expect(isMatchConfig(noContentExample)).toBeFalsy()
+
+    const noDescriptionExample = { content: ['233'] }
+    expect(isMatchConfig(noDescriptionExample)).toBeFalsy()
+
+    const emptyContentExample = { ...noContentExample, content: [] }
+    expect(isMatchConfig(emptyContentExample)).toBeFalsy()
+
+    const wrongDescriptionExample = { ...noDescriptionExample, description: 233 }
+    expect(isMatchConfig(wrongDescriptionExample)).toBeFalsy()
+
+    const wrongContentExample = { ...noContentExample, content: ['233', 233] }
+    expect(isMatchConfig(wrongContentExample)).toBeFalsy()
+
+    const rightExample = { ...noContentExample, ...noDescriptionExample }
+    expect(isMatchConfig(rightExample)).toBeTruthy()
+  })
+})
