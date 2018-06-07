2bcc880afb6df3c886c657fdc797fdf735d725d4
Yuyang Zhang <helloqiu95@gmail.com>
2018-06-07 14:19:05 +0800

    feat(robot): initial implement

--- src/robot.ts
+++ src/robot.ts
@@ -1,5 +1,20 @@
 import { Robot } from 'probot'
+import { parseConfig, shouldClose } from './parser'
+import { closeIssue, createComment } from './api'
+
+const configPath = process.env.WEROBOT_BOT_CONFIG_PATH
+if (configPath === undefined) {
+  throw new Error("Can't get WEROBOT_BOT_CONFIG_PATH")
+}
+
+const config = parseConfig(configPath)
 
 export = (robot: Robot) => {
-  robot.log('Yay, the app was loaded!')
+  robot.on('issues.opened', async context => {
+    const issueBody = context.payload.issue.body
+    if (!shouldClose(config, issueBody)) {
+      await createComment(context, config.comment)
+      await closeIssue(context)
+    }
+  })
 }
